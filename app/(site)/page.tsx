0ffdf2a98808b3886b77118e9cbdb67142fd377b
Unclephil <philipnssien@gmail.com>
2024-01-17 22:48:23 +0100

    worked on Header issues

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -20,7 +20,7 @@ export default async function Home() {
     overflow-y-auto
    "
     >
-      <Header>
+      <Header songs={[]}>
         <div className="mb-2">
           <h1 className="text-white text-3xl font-semibold">Welcome back</h1>
           <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 mt-4">
